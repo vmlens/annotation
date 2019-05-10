504725d4d1b47430ad3e455fcb634d7a182f5567
Thomas Krieger <thomas.krieger@vmlens.com>
2019-05-10 14:22:32 +0200

    interleave annotation

---
+++ src/main/java/com/vmlens/api/Agent.java
@@ -0,0 +1,18 @@
+package com.vmlens.api;
+
+public class Agent {
+
+	
+	public static void deactivateAtomic(Class cl)
+	{
+		
+	}
+	
+	
+	public static void activateAtomic(Class cl)
+	{
+		
+	}
+	
+	
+}
