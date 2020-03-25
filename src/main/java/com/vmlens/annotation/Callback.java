526b347e8a9ad0be77ed3e57947b65b3cfe0041c
Thomas Krieger <thomas.krieger@vmlens.com>
2020-03-25 11:33:57 +0100

    version 1.0.4

--- src/main/java/com/vmlens/annotation/Callback.java
+++ src/main/java/com/vmlens/annotation/Callback.java
@@ -5,6 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks that a method of an atomic class calls a callback method. 
+ * 
+ * @see Atomic
+ * 
+ */
+
+
 @Target(value={ElementType.METHOD})
 @Retention(value=RetentionPolicy.CLASS)
 public @interface Callback {
--- src/main/java/com/vmlens/annotation/Stateless.java
+++ src/main/java/com/vmlens/annotation/Stateless.java
@@ -5,6 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 
+ * Marks this class as stateless.
+ * 
+ * 
+ */
+
+
 @Target(value={ElementType.TYPE})
 @Retention(value=RetentionPolicy.CLASS)
 public @interface Stateless {
